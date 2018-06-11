3ade25f095ea9b9fd16d83c58ce6da0950f07e97
DaveLindberg <dave@davelindberg.com>
2018-06-11 06:05:59 -0400

    Refactored footer, needs cleanup

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,101 +4,9 @@ import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import styled from 'styled-components';
 import Header from '../components/Header/header.js';
-import { FaLinkedin, FaGithub } from 'react-icons/lib/fa';
+import Footer from '../components/Footer/footer.js';
 
 // import './index.css';
-
-const FooterContent = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 2fr 2fr 1fr;
-  grid-template-rows: auto auto;
-  grid-gap: 20px;
-  background-color: #eceaea;
-  padding: 1rem 2rem 2rem;
-  font-size: 80%;
-  line-height: 150%;
-  a {
-    text-decoration: none;
-    color: #cc9900;
-    :hover {
-      color: #999900;
-    }
-  }
-`;
-
-const FooterContentBrand = styled.div`
-  grid-column: 2 / 2;
-`;
-
-const FooterLink = props => (
-  <li
-    style={{
-      display: 'block',
-      marginRight: '1rem'
-    }}
-  >
-    <Link
-      to={props.to}
-      style={{
-        textDecoration: 'none',
-        color: '#CC9900'
-      }}
-    >
-      {props.children}
-    </Link>
-  </li>
-);
-
-const Footer = () => (
-  <FooterContent>
-    <p
-      style={{
-        gridColumnStart: 2,
-        gridColumnEnd: 4,
-        marginTop: '1em',
-        marginBottom: 0
-      }}
-    >
-      &copy; 2018 Dave Lindberg Marketing & Design{' '}
-    </p>
-    <FooterContentBrand>
-      <FooterLink to="/" className="home">
-        Home
-      </FooterLink>
-      <FooterLink to="/#marketing">Marketing</FooterLink>
-      <FooterLink to="/#design">Design</FooterLink>
-      <FooterLink to="/#contact">Contact</FooterLink>
-    </FooterContentBrand>
-    <div>
-      <p>
-        <FaLinkedin color="#999900" />
-        <a href="https://www.linkedin.com/in/davelindberg/" target="blank">
-          {' '}
-          /DaveLindberg
-        </a>
-      </p>
-      <p>
-        <FaGithub color="#999900" />
-        <a href="https://github.com/DaveLindberg" target="blank">
-          {' '}
-          /DaveLindberg
-        </a>
-      </p>
-    </div>
-    <p
-      style={{
-        fontSize: '80%',
-        lineHeight: '150%',
-        gridColumnStart: 2,
-        gridColumnEnd: 4
-      }}
-    >
-      All brands and images are property of their respective owners. Additional
-      photos courtesy Hermes Rivera, Nick Seliverstov and Mario Purisic via
-      Unsplash.
-    </p>
-  </FooterContent>
-);
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -128,8 +36,7 @@ const TemplateWrapper = ({ children }) => (
       />
       <Header
         style={{
-          gridRowStart: 1,
-          gridRowEnd: 2
+          gridRow: 1
         }}
       />
       <div
@@ -138,13 +45,17 @@ const TemplateWrapper = ({ children }) => (
           margin: '0 auto',
           /* maxWidth: 960,*/
           padding: '0px 0rem 0rem',
-          gridRowStart: 2,
-          gridRowEnd: 3
+          gridRow: 2
         }}
       >
         {children()}
       </div>
-      <Footer />
+      <Footer
+        className="Footer"
+        style={{
+          gridRow: 3
+        }}
+      />
     </div>
   </div>
 );
